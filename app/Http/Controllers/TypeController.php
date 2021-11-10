3a35a994c354d62f3d4f49bc1cfdae0f87398faa
MatasVaitkevicius <vaitkevicius.matas@gmail.com>
2021-11-10 20:32:51 +0200

    Updates

--- app/Http/Controllers/TypeController.php
+++ app/Http/Controllers/TypeController.php
@@ -14,6 +14,7 @@ class TypeController extends Controller
     public function __construct()
     {
         $this->middleware('auth:api');
+        $this->middleware('auth.role:admin,user');
     }
 
     /**
